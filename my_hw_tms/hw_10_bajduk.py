500670a949608924aeebb6e162199a619a348485
DisasterCooper <bajdukmax@gmail.com>
2023-03-30 12:58:18 +0300

    Add hw 10 task3

--- my_hw_tms/hw_10_bajduk.py
+++ my_hw_tms/hw_10_bajduk.py
@@ -70,3 +70,100 @@ car1 = Car('BMW', 7, 'x5', 260)
 car2 = Car('Porsche', 1, '911 Turbo S', 330)
 car1.move()
 car2.move()
+
+print('hw_10_task_3*')
+
+
+class Contact:
+
+    def __init__(self, name: str, phone: str) -> None:
+        self.name = name
+        self.phone = phone
+
+    def __str__(self):
+        return f'{self.name}: {self.phone}'
+
+    def __repr__(self):
+        return f'Contact({self.name}, {self.phone})'
+
+
+class PhoneBook:
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+        with open(file_name) as f:
+            list_of_dict = json.load(f)
+            self.contacts: list[Contact] = [
+                Contact(**contact_dict) for contact_dict in list_of_dict
+            ]
+
+    def search_cont_name(self, name: str) -> Contact | None:
+        for cont in self.contacts:
+            if cont.name == name:
+                return cont
+
+    def search_cont_phone(self, phone: str) -> Contact | None:
+        for cont in self.contacts:
+            if cont.name == phone:
+                return cont
+
+    def load_contacts(self, cont: Contact) -> Contact | None:
+        if not self.search_cont_phone(cont.name) or not self.search_cont_phone(cont.phone):
+            self.contacts.append(cont)
+            return cont
+
+    def remove_contacts(self, cont: Contact) -> Contact | None:
+        if cont is not None:
+            self.contacts.remove(cont)
+            return cont
+
+    def remove_cont_name(self, name: str) -> Contact | None:
+        cont = self.search_cont_name(name)
+        return self.remove_contacts(cont)
+
+    def remove_cont_phone(self, phone: str) -> Contact | None:
+        cont = self.search_cont_phone(phone)
+        return self.remove_contacts(cont)
+
+    def save_contacts_file(self):
+        with open(self.file_name, 'w') as f:
+            json.dump(self.contacts, f, indent=4, default=vars)
+
+
+my_contacts = PhoneBook("my_contacts.json")
+contacts = [
+    Contact("A", "+1111111"),
+    Contact("B", "+2222222"),
+    Contact("C", "+3333333"),
+    Contact("E", "+4444444"),
+    Contact("E", "+4444444"),
+    ]
+for cont in contacts:
+    load_cont = my_contacts.load_contacts(cont)
+    print(f'{cont} is added') if load_cont else print(f'{cont} is already in my_contacts')
+
+
+phones = ["+1111111", "+6666666"]
+for phone in phones:
+    cont = my_contacts.search_cont_phone(phone)
+    if cont is not None:
+        print(f'search phone is {cont}')
+    else:
+        print(f'{phone} not found')
+
+
+names = ["T", "A"]
+for name in names:
+    cont = my_contacts.search_cont_name(name)
+    if cont is not None:
+        print(f'search phone is {cont}')
+    else:
+        print(f'{name} not found')
+
+
+print(my_contacts.contacts)
+my_contacts.save_contacts_file()
+
+
+cont = Contact("B", "+2222222")
+print(vars(cont))
+print(cont.__dict__)
