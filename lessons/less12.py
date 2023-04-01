cd92a23ba51d0599105ea5f43a00c0216d74da53
DisasterCooper <bajdukmax@gmail.com>
2023-04-01 20:09:12 +0300

    Upd lesson 12

--- lessons/less12.py
+++ lessons/less12.py
@@ -3,13 +3,70 @@ from dataclasses import dataclass
 
 # Magic methods
 
-# name mangling, искажение имен
+# name mangling, искажение имен:  __name (at least two leading underscores,
+# # # at most one trailing underscore)
+
 
 # __update - можно вызвать только через <class>__update
 # если не хотим, чтобы что-то сломали -> при наследовании
 
+class Mapping:
+    def __init__(self, iterable):
+        self.items_list = []
+        self.__update(iterable)
 
-# @dataclass # будет сравнивать все, что в init
+    def update(self, iterable):
+        for item in iterable:
+            self.items_list.append(item)
+
+    __update = update  # private copy of original update() method
+
+
+#
+try:
+    mapping = Mapping([1, 2, 3])
+except ValueError:
+    pass
+
+
+# print(mapping._Mapping__update([4, 5, 6]))
+# print(mapping.items_list)
+#
+#
+# class MappingSubclass(Mapping):
+#
+#     def update(self, keys, values):
+#         # provides new signature for update()
+#         # but does not break __init__()
+#         for item in zip(keys, values):
+#             self.items_list.append(item)
+#
+#
+# mapping_subclass = MappingSubclass([1, 2, 3])
+# print(mapping_subclass.items_list)
+# print(MappingSubclass.__mro__)
+#
+# # о каких magic methods важно знать
+# # __init__
+#
+# # __str__ - человекочитаемый вид, на свое усмотрение
+# # __repr__ - 'MappingSubclass([1, 2, 3])', для программистов
+#
+# # __eq__(==), __ne__(!=), __lt__(<), __le__(<=), __gt__(>), __ge__(>=) - @functools.total_ordering
+# # __hash__
+# # Если класс не определяет метод __eq__(), он также не должен определять операцию __hash__();
+# # если он определяет __eq__(), но не __hash__(), его экземпляры нельзя будет использовать
+# # в качестве элементов в хешируемых коллекциях (словарь и множество).
+# Если класс определяет изменяемые объекты и реализует метод __eq__(),
+# он не должен реализовывать __hash__(), так как реализация хешируемых коллекций требует,
+# чтобы хеш-значение ключа было неизменным.
+# # Пользовательские классы по умолчанию имеют методы __eq__() и __hash__();
+# # с ними все объекты сравниваются как неравные (кроме самих себя),
+# # и x.__hash__() возвращает соответствующее значение, такое что x == y подразумевает,
+# # что x равно y и hash(x) == hash(y).
+
+
+@dataclass  # будет сравнивать все, что в init
 class User:
     # name: str
     def __init__(self, name):
@@ -23,6 +80,7 @@ print(id(user))
 print(id(user2))
 print(user == user2)  # если dataclass - магический метод сделает True
 print(user == user3)
+
 
 # __eq__ (==), __ne__(!=), ... - @functools.total_ordering
 
@@ -38,6 +96,8 @@ class Point:
 
 p1 = Point(1, 1)
 print(p1.x, p1.y)
+
+
 # p1.z = 5  # выдаст ошибку
 
 
@@ -55,9 +115,67 @@ magic()  # метод call дает вызвать такой экземпляр
 print(callable(magic))  # проверяет метод на "магичность =)"
 
 # декоратор класса
+from functools import wraps
+from typing import Any, Callable
+
+
+def validate_one_int_arg(func: Callable) -> Callable:
+    @wraps(func)
+    def inner(number: int) -> Any:  # заменяет декорируемую функцию (в нашем случае это is_even)
+        if type(number) != int:
+            print('Ошибка! Введенные данные не являются числом')
+            return
+        return func(number)
+    return inner  # inner - это объект типа функция
+
+
+from typing import Any
+
+
+class validate_args:
+    def __init__(self, validation_type):
+        self.validation_type = validation_type
+
+    def __call__(self, func):
+        def new_func(number: Any):
+            if type(number) != self.validation_type:
+                print('Ошибка! Введенные данные не являются числом')
+                return
+            return func(number)
+        return new_func
+
+
+def validate_args(validation_type, some_str):
+    def decorator(func):
+        def new_func(number: Any):  # эта функция заменит наши is_even и do_smth
+            if type(number) != validation_type:
+                print('Ошибка! Введенные данные не являются числом')
+                return
+            print(f"валидация {some_str} прошла успешно!")
+            return func(number)
+        return new_func
+    return decorator
+
+
+@validate_args(int, "is_even validation")
+def is_even(number: int) -> bool | None:
+    return number % 2 == 0
+
+
+@validate_args(float, "do_smth validation")
+def do_smth(number: float) -> None:
+    print(number)
+
 
 # decorator = validate_args(int)
 # is_even = decorator(is_even)  # то же самое что is_even = validate_args(int)(is_even)
+
+result = is_even(10)
+print(result)
+result2 = is_even('10')
+do_smth(2.2)
+do_smth('2.2')
+
 
 print('________Task________')
 import time
@@ -75,7 +193,7 @@ class process_time:
         t1 = time.time()
         result = self.func(*args, **kwargs)
         t2 = time.time()
-        print(f'Время вывода функции: {t2-t1}')
+        print(f'Время вывода функции: {t2 - t1}')
         return result
 
 
@@ -135,7 +253,7 @@ except ZeroDivisionError:
 
 num1 = 1
 try:
-    num2 = '0'    # int(input('Введите число: '))
+    num2 = '0'  # int(input('Введите число: '))
     print(num1 / num2)
     print('деление окончено')
 except ZeroDivisionError:
@@ -148,4 +266,3 @@ finally:
 # ValueError
 # TypeError
 # NameError
-
