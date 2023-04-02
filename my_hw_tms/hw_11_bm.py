390efb1f38def20875e0dc682212637c106dbafa
DisasterCooper <bajdukmax@gmail.com>
2023-04-02 19:29:47 +0300

    draft hw11 for help

--- my_hw_tms/hw_11_bm.py
+++ my_hw_tms/hw_11_bm.py
@@ -3,43 +3,80 @@
 хранить операцию (снятие или пополнение), сумму, дату и время операции.
 Создать экземпляр банковского аккаунта и проверить его работу."""
 from datetime import datetime
-from dataclasses import dataclass
 from enum import Enum
 
 print('hw_11_task1')
 
 
 class OperationEnum(Enum):
-    PLUS = 'пополнение'
-    MINUS = 'снятие'
+    PLUS = 'incoming'
+    MINUS = 'withdrawal'
 
 
-@dataclass
+class HistoryOpp:
+    def __init__(
+            self,
+            operation_enum: OperationEnum,
+            amount: float,
+            date_time: datetime,
+    ) -> None:
+        self.operation_enum = operation_enum
+        self.amount = amount
+        self.date_time = date_time
+
+    def __repr__(self):
+        return f'{self.operation_enum}, {self.amount}, {self.date_time}'
+
+
 class BankAcc:
-    name: str
-    balance: float
 
-    # history: list | None
+    def __init__(
+            self,
+            bank_acc: str,
+            balance: float,
+            # date_time: datetime,
+            history_opp: list[HistoryOpp]
+    ) -> None:
+        self.bank_acc = bank_acc
+        self.balance = balance
+        # self.date_time = datetime
+        self.history_opp = list[HistoryOpp] = []  # как передать историю, не могу понять
 
     @property
     def check_balance(self):
-        return f'Balance {self.name} = {self.balance}'
+        return f'Balance {self.bank_acc} = {self.balance}'
 
+    def __repr__(self):
+        return f'{self.bank_acc}, {OperationEnum}, {datetime},balance {self.balance}'
 
-@dataclass
-class History:
-    operation: OperationEnum
-    amount: float
-    date_time: datetime
-
-
-class Operation_with_acc:
+    def create_bank_acc(self, bank_acc: str, balance: float):
+        self.bank_acc.append(bank_acc)
+        self.balance = balance
+        if bank_acc in self.bank_acc:
+            return f'{self.bank_acc} is already exists'
 
     def income(self, amount: float):
         self.balance += amount
-        self.history.append('income', datetime.now())
+        date_time = datetime.now()
+        print(f'{date_time}')
+        self.history_opp.append(OperationEnum.PLUS, amount, date_time)
+        # print(f'{self.bank_acc}, {OperationEnum}, {datetime},balance {self.balance}')
+
+    def withdrawal(self, amount: float):
+        if self.balance < amount:
+            print('недостаточно средств')
+            return
+        self.balance -= amount
+        date_time = datetime.now()
+        print(f'{date_time}')
+        self.history_opp.append(OperationEnum.MINUS, amount, datetime)
+        # print(f'{self.bank_acc}, {OperationEnum}, {datetime},balance {self.balance}')
 
 
 user1 = BankAcc('A', 1000)
-print(user1.check_balance)
+print(user1)
+# print(user1.check_balance)
+# user1.income(200)
+# print(user1.check_balance)
+
 
